ad5f4d26e7abc7b98688415924fee28a87753eb3
syafrin <syafrinibrahim12@gmail.com>
2020-09-13 14:28:33 -0700

    create form login

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
-
+Route::match(["GET", "POST"], "/register", function(){
+ return redirect("/login");
+})->name("register");
 Route::get('/home', 'HomeController@index')->name('home');
