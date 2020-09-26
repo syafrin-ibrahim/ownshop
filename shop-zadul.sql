524c96a954a52a975a67abd48f1ef0f454d4ca36
unknown <syafrinibrahim12@gmail.com>
2020-09-26 09:31:55 +0800

    update database

--- shop-zadul.sql
+++ shop-zadul.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 26 Sep 2020 pada 03.27
+-- Waktu pembuatan: 15 Sep 2020 pada 06.50
 -- Versi server: 10.4.13-MariaDB
 -- Versi PHP: 7.4.7
 
@@ -20,64 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `shop`
 --
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `books`
---
-
-CREATE TABLE `books` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `category_id` int(10) UNSIGNED NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `author` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `publisher` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `cover` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `price` double(8,2) NOT NULL,
-  `views` int(10) UNSIGNED NOT NULL DEFAULT 0,
-  `stock` int(10) UNSIGNED NOT NULL DEFAULT 0,
-  `status` enum('PUBLISH','DRAFT') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_by` int(11) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data untuk tabel `books`
---
-
-INSERT INTO `books` (`id`, `category_id`, `title`, `slug`, `description`, `author`, `publisher`, `cover`, `price`, `views`, `stock`, `status`, `created_by`, `created_at`, `updated_at`) VALUES
-(1, 1, 'Laravel Fundamental', 'laravel-fundamental', 'BUku Laravel FUndamental', 'Azamuddin', 'Informatika', 'book/C9LrEg0wHvyxeoMO2WbjeNYhsJmTVdsKNWEJgUM3.jpeg', 230000.00, 0, 25, 'PUBLISH', 1, '2020-09-25 17:23:37', '2020-09-25 17:23:37'),
-(2, 2, 'Android Programming Java', 'android-programming-java', 'Ebook Android Java', 'GIlanng', 'Dicoding', 'book/3UIbPCkGE4RD5ZAvjhbNr8yatiI6xbc7sI7vClOx.jpeg', 500000.00, 0, 34, 'PUBLISH', 1, '2020-09-25 17:25:12', '2020-09-25 17:25:12'),
-(3, 3, 'First Ui Design', 'first-ui-design', 'Ebook Ui Design', 'Andika', 'Javamedia', 'book/fKequP2hn7RSsBSaxrd3ZkQhEhwz0tP9ZtBU26lG.jpeg', 123000.00, 0, 23, 'PUBLISH', 1, '2020-09-25 17:26:52', '2020-09-25 17:26:52');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `categories`
---
-
-CREATE TABLE `categories` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_by` int(11) NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data untuk tabel `categories`
---
-
-INSERT INTO `categories` (`id`, `name`, `slug`, `image`, `created_by`, `created_at`, `updated_at`) VALUES
-(1, 'web programming', 'web-programming', 'category/SMs9RTm2HQ4w8mCO1eJP27OCfsQVXByWh4wSSkYh.jpeg', 1, '2020-09-25 17:20:24', '2020-09-25 17:20:24'),
-(2, 'mobile programming', 'mobile-programming', 'category/iNyHhYb0luZiWqezZbmB810fbqk49Lpdo7mx800P.jpeg', 1, '2020-09-25 17:22:31', '2020-09-25 17:22:31'),
-(3, 'UI-UX', 'ui-ux', 'category/zwXnq9jAMnEeiaIT5kfAlshfAmCSGIynow2RtyJ7.jpeg', 1, '2020-09-25 17:25:55', '2020-09-25 17:25:55');
 
 -- --------------------------------------------------------
 
@@ -114,9 +56,7 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2020_09_13_191454_update_users_table', 2),
-(5, '2020_09_18_141753_create_categories_table', 3),
-(6, '2020_09_19_040744_create_books_table', 3);
+(4, '2020_09_13_191454_update_users_table', 2);
 
 -- --------------------------------------------------------
 
@@ -158,25 +98,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `username`, `roles`, `address`, `phone`, `avatar`, `status`) VALUES
-(1, 'syafrin ibrahim', 'syafrinibrahim12@gmail.com', NULL, '$2y$10$tP8B0oRN5wzegviCueOP6OFXH69QCGt.YT.NIjJOANYK5J1sjnh9q', NULL, '2020-09-14 02:49:14', '2020-09-14 02:49:14', 'syafrin12', '[\"ADMIN\"]', 'jl panjaitan', '082197215585', 'pakatuan12.png', 'ACTIVE');
+(1, 'syafrin ibrahim', 'syafrinibrahim12@gmail.com', NULL, '$2y$10$tP8B0oRN5wzegviCueOP6OFXH69QCGt.YT.NIjJOANYK5J1sjnh9q', NULL, '2020-09-14 02:49:14', '2020-09-14 02:49:14', 'syafrin12', '[\"ADMIN\"]', 'jl panjaitan', '082197215585', 'pakatuan12.png', 'ACTIVE'),
+(3, 'ahmad subardjo', 'ahmad@gmail.com', NULL, '$2y$10$f9gIazxGUdHDCZa7chYkpuvkRZHVT80ep4S1RwFiXfWFX2pAEW/pa', NULL, '2020-09-15 11:34:35', '2020-09-15 11:34:35', 'ahmad', '[\"ADMIN\"]', 'lumajang', '0821345667', 'assets/avatar/OQFL16WIeO3jL3N92uViIXCNhvSfcSdLzfycSi7i.jpeg', 'ACTIVE');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indeks untuk tabel `books`
---
-ALTER TABLE `books`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `books_category_id_foreign` (`category_id`);
-
---
--- Indeks untuk tabel `categories`
---
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `categories_slug_unique` (`slug`);
 
 --
 -- Indeks untuk tabel `failed_jobs`
@@ -209,18 +136,6 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT untuk tabel `books`
---
-ALTER TABLE `books`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT untuk tabel `categories`
---
-ALTER TABLE `categories`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
 -- AUTO_INCREMENT untuk tabel `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
@@ -230,23 +145,13 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT untuk tabel `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `books`
---
-ALTER TABLE `books`
-  ADD CONSTRAINT `books_category_id_foreign` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
